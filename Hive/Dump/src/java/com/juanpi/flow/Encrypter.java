a0c8eeb5305753de25303acaec0c02ca74526aad
xiaopang <xiaopang@juanpi.com>
2015-04-14 10:28:36 +0800

    no message

--- Hive/Dump/src/java/com/juanpi/flow/Encrypter.java
+++
@@ -1,7 +0,0 @@
-package com.juanpi.flow;
-
-public interface Encrypter {
-	
-	public String  encode(String input);	
-	public String  decode(String input);
-}
--- Hive/hive_clean/clean/__init__.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'hadoop'
-
-
-import db_manager
-import periodic_clean
